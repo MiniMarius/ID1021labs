3a90ed825bc3faf35903f79adcf861f525c2ab4c
MiniMarius <seffermarius@gmail.com>
2021-09-05 21:43:18 +0200

    test of node-based stack

--- src/lab1/IterativeRecursiveClass.java
+++ src/lab1/IterativeRecursiveClass.java
@@ -1,5 +1,65 @@
 package lab1;
 
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.NoSuchElementException;
+
 public class IterativeRecursiveClass {
+
+    public static void main(String[] args) {
+        StackClass stack = new StackClass();
+        while (!StdIn.isEmpty())
+            stack.push(StdIn.readChar());
+            StdOut.println(stack.first);
+    }
+
+    public static String recursiveReverseMethod(String string) {
+        if (string.isEmpty())
+            return string;
+        return recursiveReverseMethod(string.substring(1)) + string.charAt(0);
+    }
+
+    private static class StackClass {
+        private Node<Character> first;
+        private int n;
+
+        public StackClass() {
+            first = null;
+            n = 0;
+        }
+
+        private static class Node<Character> {
+            private Character item;
+            private StackClass.Node<Character> next;
+        }
+        private void push(Character c) {
+            StackClass.Node<Character> oldfirst = first;
+            first = new StackClass.Node<Character>();
+            first.item = c;
+            first.next = oldfirst;
+            n++;
+
+        }
+
+        private Character pop() {
+            if (isEmpty()) throw new NoSuchElementException("Stack underflow");
+            Character item = first.item;        // save item to return
+            first = first.next;            // delete first node
+            n--;
+            return item;
+
+        }
+
+        private boolean isEmpty() {
+            return first == null;
+        }
+
+        private int size() {
+            return n;
+        }
+    }
+
 
 }
--- src/lab1/iterativeRecursive.c
+++ src/lab1/iterativeRecursive.c
@@ -1,21 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 
 
-
-
+void recursiveReverse();
+void iterativeReverse();
 int main(int argc, char const *argv[])
 {
     printf("Enter your characters please: \n");
-    recursiveFunction();
+    //recursiveFunction();
+    iterativeReverse();
     return 0;
 }
 
 
-void recursiveFunction() {
-    if (getchar() != '\n')
-        recursiveFunction();
-    putchar(character)
+void recursiveReverse() {
+    int c = getchar();
+    if(c == '\n') return;
+    recursiveReverse();
+    putchar(c);
+}
+
+void iterativeReverse() {
+        char c;
+        char *str = malloc(20*sizeof(char));
+        int i = 0;
+        while((c = getchar()) != '\n')
+            str[i++] = c;
+
+        for(i = i - 1; i >= 0; i--)
+            putchar(str[i]);
+
+        free(str);
 }
