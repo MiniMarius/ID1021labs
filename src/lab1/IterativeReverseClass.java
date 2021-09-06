ba8bea8203f42fc2f80287b3095e9cad2f1c8082
MiniMarius <seffermarius@gmail.com>
2021-09-06 08:57:37 +0200

    Node char stack implemented

--- src/lab1/IterativeReverseClass.java
+++ src/lab1/IterativeReverseClass.java
@@ -6,7 +6,7 @@ import edu.princeton.cs.algs4.StdOut;
 public class IterativeReverseClass {
 
     public static void main(String[] args) {
-        StackClass stack = new StackClass();
+        StackOfChars stack = new StackOfChars();
         StdOut.print("Enter characters to be reversed");
         while (!StdIn.isEmpty())
             stack.push(StdIn.readChar());
@@ -14,31 +14,39 @@ public class IterativeReverseClass {
             StdOut.print(stack.pop());
     }
 
-    private static class StackClass {
-            private char[] array;
-            private int items;
-
-        public StackClass() {
-            array = new char[8];
-            items = 0;
+    private static class StackOfChars {
+            private Node first;
+            private int n;
+        private static class Node {
+            private char item;
+            Node next;
         }
 
-        public void push(char c) {
-            array[items] = c;
-            items++;
+        public StackOfChars() {
+            first = new Node();
+            n = 0;
+        }
+
+        public void push(char item) {
+            Node oldfirst = first;
+            first.item = item;
+            first.next = oldfirst;
         }
 
         public char pop() {
-            items--;
-            return array[items];
+            Character item = first.item;
+            first = first.next;
+            return item;
         }
 
         boolean isEmpty() {
-            return items == 0;
+            return n == 0;
         }
 
         int size() {
-            return items;
+            return n;
         }
     }
+
+
 }
