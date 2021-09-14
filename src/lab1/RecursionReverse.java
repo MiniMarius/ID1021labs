4ac3358b95e9c5cc651bc7ac0a9d30b3f020e903
MiniMarius <seffermarius@gmail.com>
2021-09-14 20:07:34 +0200

    fixed printing to fit the requirement

--- src/lab1/RecursionReverse.java
+++ src/lab1/RecursionReverse.java
@@ -9,9 +9,9 @@ public class RecursionReverse {
         char c;
         if(!StdIn.hasNextChar())
             return;
-        c = StdIn.readChar();
+        c = StdIn.readChar(); //CTRL-D if stuck in console to stop stdin from reading chars
         reverse();
-        StdOut.print(c);
+        StdOut.print("["+ c + "], " );
     }
     public static void main(String[] args) {
         StdOut.print("Enter characters to reversed\n");
