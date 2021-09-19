0d0e96530c693934c9dc9b463ed9263fd295e042
Marius S <seffermarius@gmail.com>
2021-09-19 20:17:08 +0300

    added calculation of inversions

--- src/lab2/assignment1.c
+++ src/lab2/assignment1.c
@@ -1,32 +1,49 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void printArray(int arr[], int size);
-void insertionSort(int arr[], int size);
+
+void count_inversions(int arr[], int size);
+void print_array(int arr[], int size);
+void insertion_sort(int arr[], int size);
 int main(int argc, char const *argv[])
 {
-    int arr[] = {9, 6, 4, 5, 2};
+    int arr[] = {1, 2, 5, 3, 4, 0};
     int n = sizeof(arr) / sizeof(arr[0]);
-    insertionSort(arr, n);
+    count_inversions(arr, n);
+    insertion_sort(arr, n);
     return 0;
 }
-void insertionSort(int arr[], int size) {
-    int i;
+void insertion_sort(int arr[], int size) {
+    int i, j;
+    int count_of_swaps = 0;
     for(i = 1; i < size; i++) {
-        int j = i;
-        while(j > 0 && arr[j-1]>arr[j]) {
+        j = i;
+        while(j > 0 && arr[j-1] > arr[j]) {
             int temp = arr[j-1];
             arr[j-1] = arr[j];
             arr[j] = temp;
             j -=1;
-            printArray(arr, size);
+            printf("Amount of swaps: %d\n", ++count_of_swaps);
         }
     }
+    
 }
 
-void printArray(int arr[], int size) {
+void print_array(int arr[], int size) {
     int i;
     for (i = 0; i < size; i++)
         printf("%d ", arr[i]);
     printf("\n");
+}
+
+void count_inversions(int arr[], int size) {
+    int i,j;
+    int inversion_count;
+    for(i = 0; i < size - 1; i++) {
+        for(j = i + 1; j < size; j++) {
+            if(arr[i] > arr[j])
+                inversion_count++;
+        }
+    }
+    printf("Number of inversions: %d\n", inversion_count);
 }
