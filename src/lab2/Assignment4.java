d932f1d7c8714a807ed8af4f4aa4a94a7f476cd5
MiniMarius <seffermarius@gmail.com>
2021-09-23 20:33:58 +0200

    Complete refactorings done and cleanup of code

--- src/lab2/Assignment4.java
+++ src/lab2/Assignment4.java
@@ -12,57 +12,64 @@ import java.util.Scanner;
 
 public class Assignment4 {
 
-    public static Long getInsertionSortPerformance(Integer[] a) {
+    public static Integer getInsertionSortPerformance(Integer[] a) {
         long startTime = System.currentTimeMillis();
         Insertion.sort(a);
         long endTime = System.currentTimeMillis();
-        return (endTime - startTime);
+        return (int) (endTime - startTime);
     }
 
-    public static Long getMergeSortPerformance(Integer[] a) {
+    public static Integer getMergeSortPerformance(Integer[] a) {
         long startTime = System.currentTimeMillis();
-        System.currentTimeMillis();
         Merge.sort(a);
         long endTime = System.currentTimeMillis();
-        return (endTime - startTime);
+        return (int) (endTime - startTime);
     }
 
 
-    public static Long getQuickSortPerformance(Integer[] a) {
+    public static Integer getQuickSortPerformance(Integer[] a) {
         long startTime = System.currentTimeMillis();
-        System.currentTimeMillis();
         Quick.sort(a);
         long endTime = System.currentTimeMillis();
-        return (endTime - startTime);
+        return (int) (endTime - startTime);
     }
 
     public static void outputToCsv(Long millis, String filename) {
         try (PrintWriter writer = new PrintWriter(new File(filename + ".csv"))) {
-
             StringBuilder sb = new StringBuilder();
             sb.append(millis);
             sb.append(',');
             writer.write(sb.toString());
-            System.out.println("done!");
-
+            System.out.println("file created");
         } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
         }
     }
 
-    public static void main(String[] args) throws FileNotFoundException {
-        String[] fileNameList = {"src/inputFiles/1000ints.txt", "src/inputFiles/10000ints.txt", "src/inputFiles/1000000ints.txt", "src/inputFiles/10000000ints.txt"};
-        for (String file : fileNameList) {
-            Scanner in = new Scanner(new FileReader(file));
+    public static Integer[] getInputFile(Integer inputSize) {
+        try {
+            String inputFile = "src/inputFiles/" + inputSize + "ints.txt";
+            Scanner in = new Scanner(new FileReader(inputFile));
             Integer[] numbers = new Integer[in.nextInt()];
             for (int i = 0; i < numbers.length; i++) {
                 if (in.hasNextInt()) {
                     numbers[i] = in.nextInt();
                 }
             }
-            outputToCsv(getInsertionSortPerformance(numbers), "insertion");
-            outputToCsv(getQuickSortPerformance(numbers), "quicksort");
-            outputToCsv(getMergeSortPerformance(numbers), "mergesort");
+            return numbers;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Integer[] arr = getInputFile(10000);
+        Integer insertionSortPerformance = getInsertionSortPerformance(arr);
+        Integer mergeSortPerformance = getMergeSortPerformance(arr);
+        Integer quickSortPerformance = getQuickSortPerformance(arr);
+        System.out.println("average insertion time: " + insertionSortPerformance);
+        System.out.println("average merge time: " + mergeSortPerformance);
+        System.out.println("average quicksort time: " + quickSortPerformance);
     }
 }
