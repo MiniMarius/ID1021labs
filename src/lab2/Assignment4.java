1df0f4ed5f7ae89224a46ea203e6fa0dd34ed1bb
MiniMarius <seffermarius@gmail.com>
2021-09-24 08:25:52 +0200

    renaming of method, + small cleanups

--- src/lab2/Assignment4.java
+++ src/lab2/Assignment4.java
@@ -34,19 +34,17 @@ public class Assignment4 {
         return (int) (endTime - startTime);
     }
 
-    public static void outputToCsv(Long millis, String filename) {
+    public static void outputToCsv(Integer millis, String filename) {
         try (PrintWriter writer = new PrintWriter(new File(filename + ".csv"))) {
-            StringBuilder sb = new StringBuilder();
-            sb.append(millis);
-            sb.append(',');
-            writer.write(sb.toString());
-            System.out.println("file created");
+            String s = String.valueOf(millis) + ',';
+            writer.write(s);
+            System.out.println(filename + ".csv created");
         } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
         }
     }
 
-    public static Integer[] getInputFile(Integer inputSize) {
+    public static Integer[] getInputFileBySize(Integer inputSize) {
         try {
             String inputFile = "src/inputFiles/" + inputSize + "ints.txt";
             Scanner in = new Scanner(new FileReader(inputFile));
@@ -64,12 +62,15 @@ public class Assignment4 {
     }
 
     public static void main(String[] args) {
-        Integer[] arr = getInputFile(10000);
+        Integer[] arr = getInputFileBySize(1000);
         Integer insertionSortPerformance = getInsertionSortPerformance(arr);
         Integer mergeSortPerformance = getMergeSortPerformance(arr);
         Integer quickSortPerformance = getQuickSortPerformance(arr);
         System.out.println("average insertion time: " + insertionSortPerformance);
         System.out.println("average merge time: " + mergeSortPerformance);
         System.out.println("average quicksort time: " + quickSortPerformance);
+        outputToCsv(insertionSortPerformance, "insertion");
+        outputToCsv(mergeSortPerformance, "mergesort");
+        outputToCsv(quickSortPerformance, "quicksort");
     }
 }
