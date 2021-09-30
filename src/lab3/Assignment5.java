39c247b7d659839e1d98d910b49f762b1f4c0933
MiniMarius <seffermarius@gmail.com>
2021-09-30 13:49:35 +0200

    implemented javas built in hashmap for checking of balance

--- src/lab3/Assignment5.java
+++ src/lab3/Assignment5.java
@@ -1,2 +1,41 @@
-package lab3;public class Assignment5 {
+package lab3;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Assignment5 {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        System.setIn(new FileInputStream("src/lab3/theText.txt"));
+        int distinct = 0, words = 0;
+        Map<String, Integer> st = new HashMap<>();
+
+        // compute frequency counts
+        while (!StdIn.isEmpty() && words < 1000) {
+            String key = StdIn.readString();
+            words++;
+            if (st.containsKey(key)) {
+                st.put(key, st.get(key) + 1);
+            } else {
+                st.put(key, 1);
+                distinct++;
+            }
+        }
+        // find a key with the highest frequency count
+        String max = "";
+        st.put(max, 0);
+        for (String word : st.keySet()) {
+            if (st.get(word) > st.get(max))
+                max = word;
+        }
+
+        StdOut.println(max + " " + st.get(max));
+        StdOut.println("distinct = " + distinct);
+        StdOut.println("words    = " + words);
+    }
 }
