2d4dd3212cf105af2575ca5fe976861fbc69c022
MiniMarius <seffermarius@gmail.com>
2021-10-01 08:23:39 +0200

    added csv output

--- src/lab3/Assignment5.java
+++ src/lab3/Assignment5.java
@@ -4,43 +4,38 @@ package lab3;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Assignment5 {
 
+
+    private static void outputSTperformance(String hashcode) {
+        try (PrintWriter writer = new PrintWriter(new FileWriter("a5distribution" + ".csv", true))) {
+            writer.write(hashcode);
+            writer.println();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) throws FileNotFoundException {
         System.setIn(new FileInputStream("src/lab3/theText.txt"));
-        int distinct = 0, words = 0;
         Map<String, Integer> map = new HashMap<>();
 
         // compute frequency counts
         while (!StdIn.isEmpty()) {
             String key = StdIn.readString();
-            words++;
             if (map.containsKey(key)) {
                 map.put(key, map.get(key) + 1);
             } else {
                 map.put(key, 1);
-                distinct++;
             }
         }
-        // find a key with the highest frequency count
-        String max = "";
-        map.put(max, 0);
-        for (String word : map.keySet()) {
-            if (map.get(word) > map.get(max))
-                max = word;
-        }
-
         for (String word : map.keySet()) {
             System.out.println("Hash code is: " + word.hashCode());
+            outputSTperformance(String.valueOf(word.hashCode()));
         }
-
-        StdOut.println(max + " " + map.get(max));
-        StdOut.println("distinct = " + distinct);
-        StdOut.println("words    = " + words);
     }
 }
