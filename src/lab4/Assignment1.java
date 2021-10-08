59562cce3a876b55af990a514fd479e541211e45
Marius S <seffermarius@gmail.com>
2021-10-08 08:55:48 +0300

    fixed git error

--- src/lab4/Assignment1.java
+++ src/lab4/Assignment1.java
@@ -7,9 +7,9 @@ public class Assignment1 {
     public static void main(String[] args) {
         try {
             SymbolGraph sg = new SymbolGraph("usa.txt", " ");
+            System.out.println(sg);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-
     }
 }
--- src/lab4/SymbolGraph.java
+++ src/lab4/SymbolGraph.java
@@ -1,6 +1,5 @@
 package lab4;
 
-import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.ST;
 
 import java.io.FileNotFoundException;
@@ -8,24 +7,14 @@ import java.io.FileReader;
 import java.util.Scanner;
 
 public class SymbolGraph {
-<<<<<<< HEAD
     private ST<String, Integer> st;
     private String[] keys;
     private GraphClass graph;
-=======
-    private ST<String, Integer> st;  // string -> index
-    private String[] keys;           // index  -> string
-    private GraphClass graph;             // the underlying graph
->>>>>>> ab52166280d23098e9e3ba69e4008d9ce7536ada
 
     public SymbolGraph(String filename, String delimiter) throws FileNotFoundException {
         st = new ST<String, Integer>();
         Scanner in = new Scanner(new FileReader("src/lab4/" + filename));
-<<<<<<< HEAD
         while (in.hasNextLine()) {
-=======
-        while (in.hasNext()) {
->>>>>>> ab52166280d23098e9e3ba69e4008d9ce7536ada
             String[] a = in.nextLine().split(delimiter);
             for (int i = 0; i < a.length; i++) {
                 if (!st.contains(a[i]))
@@ -39,13 +28,8 @@ public class SymbolGraph {
         }
 
         graph = new GraphClass(st.size());
-<<<<<<< HEAD
         in = new Scanner(new FileReader("src/lab4/" + filename));
         while (in.hasNextLine()) {
-=======
-        in = new Scanner(filename);
-        while (in.hasNext()) {
->>>>>>> ab52166280d23098e9e3ba69e4008d9ce7536ada
             String[] a = in.nextLine().split(delimiter);
             int v = st.get(a[0]);
             for (int i = 0; i < a.length; i++) {
@@ -63,20 +47,15 @@ public class SymbolGraph {
         return st.get(s);
     }
 
-<<<<<<< HEAD
-=======
     public int indexOf(String s) {
         return st.get(s);
     }
 
->>>>>>> ab52166280d23098e9e3ba69e4008d9ce7536ada
     public String name(int v) {
         validateVertex(v);
         return keys[v];
     }
 
-<<<<<<< HEAD
-=======
     public String nameOf(int v) {
         validateVertex(v);
         return keys[v];
@@ -86,7 +65,6 @@ public class SymbolGraph {
         return graph;
     }
 
->>>>>>> ab52166280d23098e9e3ba69e4008d9ce7536ada
     public GraphClass graph() {
         return graph;
     }
@@ -94,6 +72,6 @@ public class SymbolGraph {
     private void validateVertex(int v) {
         int V = graph.V();
         if (v < 0 || v >= V)
-            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
+            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V - 1));
     }
 }
