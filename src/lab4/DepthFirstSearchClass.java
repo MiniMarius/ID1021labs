27dbf7689b83e408b131a7a0a0a9680b770f28ea
Marius S <seffermarius@gmail.com>
2021-10-10 15:53:33 +0300

    added method to get path from source vertex to target vertex

--- src/lab4/DepthFirstSearchClass.java
+++ src/lab4/DepthFirstSearchClass.java
@@ -1,34 +1,52 @@
 package lab4;
 
+import edu.princeton.cs.algs4.Stack;
+
 public class DepthFirstSearchClass {
-    private boolean[] marked;
-    private int count;
+    private boolean[] marked;    // marked[v] = is there an s-v path?
+    private int[] edgeTo;        // edgeTo[v] = last edge on s-v path
+    private final int s;         // source vertex
 
     public DepthFirstSearchClass(GraphClass G, int s) {
+        this.s = s;
+        edgeTo = new int[G.V()];
         marked = new boolean[G.V()];
         validateVertex(s);
         dfs(G, s);
     }
 
+    // depth first search from v
     private void dfs(GraphClass G, int v) {
-        count++;
         marked[v] = true;
         for (int w : G.adj(v)) {
             if (!marked[w]) {
+                edgeTo[w] = v;
                 dfs(G, w);
             }
         }
     }
 
-    public boolean marked(int v) {
+    public boolean hasPathTo(int v) {
         validateVertex(v);
         return marked[v];
     }
 
-    public int count() {
-        return count;
+    /**
+     * Returns a path between the source vertex and vertex v
+     *
+     * @param v the vertex to find a path to
+     */
+    public Iterable<Integer> pathTo(int v) {
+        validateVertex(v);
+        if (!hasPathTo(v)) return null;
+        Stack<Integer> path = new Stack<Integer>();
+        for (int x = v; x != s; x = edgeTo[x])
+            path.push(x);
+        path.push(s);
+        return path;
     }
 
+    // throw an IllegalArgumentException unless {@code 0 <= v < V}
     private void validateVertex(int v) {
         int V = marked.length;
         if (v < 0 || v >= V)
