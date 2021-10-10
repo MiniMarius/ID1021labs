d5b894c50d231edef4cc4305b454747db64919a8
Marius S <seffermarius@gmail.com>
2021-10-10 15:59:49 +0300

    now correctly prints out paths from requested index

--- src/lab4/Assignment1.java
+++ src/lab4/Assignment1.java
@@ -8,10 +8,16 @@ public class Assignment1 {
     public static void main(String[] args) {
         try {
             SymbolGraph sg = new SymbolGraph("usa.txt", " ");
-            String place = "FL";
-            //DepthFirstSearchClass df = new DepthFirstSearchClass(sg.graph(), sg.index(place));
-            for (int w : sg.graph().adj(sg.index(place)))
-                System.out.println(("   " + sg.name(w)));
+            String place = "TN";
+            DepthFirstSearchClass search = new DepthFirstSearchClass(sg.graph(), sg.index(place));
+            for (int v = 0; v < sg.graph().V(); v++) {
+                System.out.println((place + " to " + sg.name(v) + ": "));
+                if (search.hasPathTo(v))
+                    for (int x : search.pathTo(v))
+                        if (x == sg.index(place)) System.out.print(x);
+                        else System.out.print("-" + x);
+                System.out.println();
+            }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
