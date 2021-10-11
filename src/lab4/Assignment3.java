d6a55705879ecfb8d316f405428672066a9f10cd
MiniMarius <seffermarius@gmail.com>
2021-10-11 13:36:51 +0200

    added necessary classes for directed graphs

--- src/lab4/Assignment3.java
+++ src/lab4/Assignment3.java
@@ -1,2 +1,10 @@
-package lab4;public class Assignment3 {
+package lab4;
+
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Assignment3 {
+    public static void main(String[] args) {
+
+    }
 }
