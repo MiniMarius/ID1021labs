4f46747f4f66b685f0ffbb3a15a477fd0177d61c
MiniMarius <seffermarius@gmail.com>
2021-10-11 19:22:03 +0200

    added file for assignment 5

---
+++ src/lab4/DirectedCycle.java
@@ -0,0 +1,57 @@
+package lab4;
+
+import edu.princeton.cs.algs4.Stack;
+
+public class DirectedCycle {
+    private boolean[] marked;        // has vertex v been marked?
+    private int[] edgeTo;
+    private boolean[] onStack;
+    private Stack<Integer> cycle;    // directed cycle (or null if no such cycle)
+
+    /**
+     * Determines whether the digraph has a directed cycle
+    */
+    public DirectedCycle(DirectedGraph G) {
+        marked  = new boolean[G.V()];
+        onStack = new boolean[G.V()];
+        edgeTo  = new int[G.V()];
+        for (int v = 0; v < G.V(); v++)
+            if (!marked[v] && cycle == null) dfs(G, v);
+    }
+
+    // run DFS and find a directed cycle (if one exists)
+    private void dfs(DirectedGraph G, int v) {
+        onStack[v] = true;
+        marked[v] = true;
+        for (int w : G.adj(v)) {
+
+            // short circuit if directed cycle found
+            if (cycle != null) return;
+
+                // found new vertex, so recur
+            else if (!marked[w]) {
+                edgeTo[w] = v;
+                dfs(G, w);
+            }
+
+            // trace back directed cycle
+            else if (onStack[w]) {
+                cycle = new Stack<Integer>();
+                for (int x = v; x != w; x = edgeTo[x]) {
+                    cycle.push(x);
+                }
+                cycle.push(w);
+                cycle.push(v);
+            }
+        }
+        onStack[v] = false;
+    }
+
+    public boolean hasCycle() {
+        return cycle != null;
+    }
+
+    public Iterable<Integer> cycle() {
+        return cycle;
+    }
+}
