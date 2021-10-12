40fe98177f07e520737b8d417f39320694a3695b
MiniMarius <seffermarius@gmail.com>
2021-10-12 08:33:43 +0200

    removed unnecessary user input

--- src/lab4/Assignment5.java
+++ src/lab4/Assignment5.java
@@ -8,14 +8,8 @@ import java.util.Scanner;
 public class Assignment5 {
     public static void main(String[] args) {
         try {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("starting location: ");
-            String place = scanner.next();
-            System.out.println("destination: ");
-            String destination = scanner.next();
             DirectedSymbolGraph sg = new DirectedSymbolGraph("usa.txt", " ");
             DirectedCycle search = new DirectedCycle(sg.digraph());
-            System.out.println((place + " to " + sg.name(sg.index(destination)) + ": "));
             if (search.hasCycle()) {
                 StdOut.print("Directed cycle: ");
                 for (int v : search.cycle()) {
